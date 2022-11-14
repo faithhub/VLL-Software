99db99f5aa0853059324f9ac70bb166b820bf93a
faithhub <adebayooluwadara@gmail.com>
2022-11-14 21:52:20 +0100

    Update from home page

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/test', function () {
-    return view('test');
-});
+// Route::get('/test', function () {
+//     return view('test');
+// });
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
