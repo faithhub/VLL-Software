66edca8ccf80bf299b22b3c46a1a911436548084
faithhub <adebayooluwadara@gmail.com>
2023-03-20 00:03:11 +0100

    Update from Admin creating Sub Admins

--- resources/views/emails/users/user.blade.php
+++ resources/views/emails/users/user.blade.php
@@ -4,18 +4,16 @@ Dear <b>{{$name}}</b>,
 
 Welcome to Virtual Law Library!
 
-We’re thrilled to have you as a partner in providing you with additional sales channels.
+We're thrilled to have you as a member of our community, and we hope that you are excited about the future of legal research.
 
-Virtual Law Library is a platform for law students and professionals. It provides instant access to a large collection of law books and case law.
+Virtual Law Library is a legal resource for law students and professionals. It provides instant access to large law books and case law collections.
 
-Our promise to you is a commitment to your growth, by being transaction transparent, and providing visibility to your product.
-
-Please don't hesitate to contact us if you have any questions or comments, we provide 24 hours, 7 days a week support service. All you need to do is simply click help once signed into your account and our representative will be there to help you.
-Cheers to legacy and have a wonderful day!
+We offer a variety of ways for you to search for what you need: please visit our bookshop for a complete list. Whether you want to explore traditional torts or read about liability rules in the 21st century, we've got something for everyone. Please don't hesitate to contact us if you have any questions or comments, we provide 24 hours, 7 days a week support service. All you need to do is simply click help once signed into your account and our representative will be there to help you.
+Cheers and have a wonderful day!
 
 <b>
  Signed:<br>
- CEO
+Account Manager
 </b>
 
 @endcomponent
