59be8d5e7ffd72cfd86d819f1a814ecdd33f722e
faithhub <adebayooluwadara@gmail.com>
2023-09-24 21:43:12 +0100

    Updates

--- resources/views/dashboard/user/view-material.blade.php
+++ resources/views/dashboard/user/view-material.blade.php
@@ -498,7 +498,7 @@
         try {
             document.addEventListener("adobe_dc_view_sdk.ready", function() {
                 var adobeDCView = new AdobeDC.View({
-                    clientId: "0dd10458f61848678f7610abd9bf8b09",
+                    clientId: "6dfef512955e46eca21b5e9545cd9ce5",
                     // clientId: "361dd918518a4899a875ef8455d503fa",
                     divId: "adobe-dc-view"
                 });
